9971329e06fe1a1b95918a5cb9a98bbf33655eb2
patarajarin <patara1919@gmail.com>
2025-01-24 18:33:59 +0700

    edit product styles header

--- src/app/production/Header.tsx
+++ src/app/production/Header.tsx
@@ -6,7 +6,7 @@ type Props= {
 
 export default function ProductHeader({isLoading}:Props) {
   return (
-        <div className={` duration-1000 text-3xl font-bold ${isLoading ? 'text-white  bg-gray-900 shadow-gray-900 shadow-md  w-3/4 text-center py-4 rounded-lg':'opacity-0 px-0'}`}>
+        <div className={` duration-1000 text-3xl font-bold ${isLoading ? 'text-white  bg-gray-900  w-3/4 text-center py-4 rounded-lg':'opacity-0 px-0'}`}>
           Production
         </div>
 
