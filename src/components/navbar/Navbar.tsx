cf8859c108c8bd6546dd8ea3a186e2e2676de87c
patarajarin <patara1919@gmail.com>
2025-01-26 02:59:10 +0700

    update useRef dropdown in navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,19 +1,34 @@
 "use client";
 import { usePathname, useRouter } from "next/navigation";
-import React, { useCallback, useEffect, useState } from "react";
+import React, { useCallback, useEffect, useRef, useState } from "react";
 import NavLink from "./NavLink";
 import Link from "next/link";
 import { Session } from "@/resource/model";
 import axios from "axios";
 import { toast } from "react-toastify";
-import {  UserRound } from "lucide-react";
+import { UserRound } from "lucide-react";
 
 export default function Navbar() {
   const pathname = usePathname();
   const router = useRouter();
   const [session, setSession] = useState<Session | null>(null);
   const [dropmenu, setDropmenu] = useState<boolean>(false);
+  const dropdownRef = useRef<HTMLDivElement>(null);
 
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        dropdownRef.current &&
+        !dropdownRef.current.contains(event.target as Node)
+      ) {
+        setDropmenu(false); 
+      }
+    };
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
   const getUserSession = useCallback(async () => {
     try {
       const res = await axios.get<{ data: Session }>("/api/users/me");
@@ -26,16 +41,16 @@ export default function Navbar() {
     } catch (error) {
       console.error("Failed to fetch session:", error);
     }
-  }, []); 
+  }, []);
   useEffect(() => {
     getUserSession();
-  }, [getUserSession]); 
+  }, [getUserSession]);
 
   const logout = async () => {
     try {
       await axios.get("/api/users/logout");
       setSession(null);
-      toast.info("logout success")
+      toast.info("logout success");
       router.push("/");
     } catch (error) {
       console.log(error instanceof Error ? error.message : "Unknown error");
@@ -45,58 +60,90 @@ export default function Navbar() {
   return (
     <div className="w-full top-0 left-0 z-40 fixed bg-gradient-to-l from-blue-600 to-gray-900 shadow-sm shadow-gray-900 px-5">
       <div className="grid lg:grid-cols-[20%_50%_30%] grid-cols-[20%_70%_10%] text-white">
-        
         {/* Left Side: Logo */}
         <Link href={"/"} className="flex gap-3 items-center lg:pl-10 py-1">
-          <img src="/assets/iconweb_white.png" width={50} height={45} alt="icon" />
-          <div className="font-extrabold text-5xl lg:block md:block hidden">IoT</div>
+          <img
+            src="/assets/iconweb_white.png"
+            width={50}
+            height={45}
+            alt="icon"
+          />
+          <div className="font-extrabold text-5xl lg:block md:block hidden">
+            IoT
+          </div>
         </Link>
-  
-  
+
         <div className="flex justify-center items-center">
-          <NavLink href="/documents" label="Document" isActive={pathname === "/documents"} />
-          <NavLink href="/production" label="Product" isActive={pathname === "/production"} />
-          <NavLink href="/devices" label="Device" isActive={pathname === "/devices"} />
- 
-          <NavLink href="/aboutus" label="About" isActive={pathname === "/aboutus"} />
+          <NavLink
+            href="/documents"
+            label="Document"
+            isActive={pathname === "/documents"}
+          />
+          <NavLink
+            href="/production"
+            label="Product"
+            isActive={pathname === "/production"}
+          />
+          <NavLink
+            href="/devices"
+            label="Device"
+            isActive={pathname === "/devices"}
+          />
+
+          <NavLink
+            href="/aboutus"
+            label="About"
+            isActive={pathname === "/aboutus"}
+          />
         </div>
 
         {session === null && pathname !== "/devices" ? (
           <div className="lg:flex justify-end items-center gap-2 md:block hidden">
-            <button className="px-10 rounded-md py-1 hover:text-white hover:bg-opacity-0 border bg-white text-blue-800 duration-300 border-white"
-              onClick={() => router.push("/authentication/login")}>
+            <button
+              className="px-10 rounded-md py-1 hover:text-white hover:bg-opacity-0 border bg-white text-blue-800 duration-300 border-white"
+              onClick={() => router.push("/authentication/login")}
+            >
               Login
             </button>
-            <button className="px-10 rounded-md duration-300 py-1  hover:bg-blue-800"
-              onClick={() => router.push("/authentication/signup")}>
+            <button
+              className="px-10 rounded-md duration-300 py-1  hover:bg-blue-800"
+              onClick={() => router.push("/authentication/signup")}
+            >
               Sign-Up
             </button>
           </div>
-        ) : session || pathname === "/devices" ?  (
+        ) : session || pathname === "/devices" ? (
           <div className="flex justify-center gap-3 items-center text-white text-xl">
-            <UserRound style={{width:"2.2rem" , height:"2.2rem"}} 
-            className="h-20 w-20 px-1 py-1 text-white bg-blue-500 rounded-full" onClick={() => setDropmenu(!dropmenu)} 
+            <UserRound
+              style={{ width: "2.2rem", height: "2.2rem" }}
+              className="h-20 w-20 px-1 py-1 text-white bg-blue-500 rounded-full"
+              onClick={() => setDropmenu(!dropmenu)}
             />
             <div className="justify-center flex items-center text-white text-sm">
               {session?.username.toUpperCase()}
             </div>
-            {/* <img src={"assets/user_gabel.jpg"} className="object-cover h-12 w-12 rounded-full" 
-              onClick={() => setDropmenu(!dropmenu)} 
-              alt="User Profile"/> */}
-            
             {dropmenu && (
-              <div className="duration-300 shadow-md shadow-gray-800 absolute translate-y-20 rounded-md px-1 py-2 bg-white grid">
-                <button className="text-center text-sm py-1 rounded-md hover:bg-slate-200 text-gray-600">Profile</button>
-                <button className="text-center text-sm py-1 rounded-md hover:bg-slate-200 text-gray-600">Edit</button>
-                <button className="text-sm py-1 px-8 rounded-md text-center hover:bg-slate-200 w-full text-red-400"
-                  onClick={logout}>
+              <div
+                ref={dropdownRef}
+                className="duration-300 shadow-md shadow-gray-800 absolute translate-y-20 rounded-md px-1 py-2 bg-white grid"
+              >
+                <button className="text-center text-sm py-1 rounded-md hover:bg-slate-200 text-gray-600">
+                  Profile
+                </button>
+                <button className="text-center text-sm py-1 rounded-md hover:bg-slate-200 text-gray-600">
+                  Edit
+                </button>
+                <button
+                  className="text-sm py-1 px-8 rounded-md text-center hover:bg-slate-200 w-full text-red-400"
+                  onClick={logout}
+                >
                   Logout
                 </button>
               </div>
             )}
           </div>
         ) : (
-          <p className="text-white">Loading...</p> 
+          <p className="text-white">Loading...</p>
         )}
       </div>
     </div>
