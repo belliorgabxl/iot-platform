3548d9a75a0e8bf794608fee2e9e040193b19956
patarajarin <patara1919@gmail.com>
2025-01-27 13:24:43 +0700

    adding motion fram package and custom homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,26 +3,32 @@ import GetstartButton from "@/components/article/getstartButton";
 import Homepage_article from "@/components/article/homePageDetail";
 import CardImageHomepage from "@/components/card/cardImageHomepage";
 import CLShomepage from "@/components/CLS/CLShomepage";
+import FadeInOnScroll from "@/components/FadInScroll";
 import { useEffect, useState } from "react";
 
 export default function Home() {
-  const [isLoading , setIsLoaded] = useState<boolean>(false)
-  useEffect(()=>{
-    setIsLoaded(true)
-  },[])
+  const [isLoading, setIsLoaded] = useState<boolean>(false);
+  useEffect(() => {
+    setIsLoaded(true);
+  }, []);
   return (
-    <div className=" w-full pb-40 bg-gradient-to-tr from-gray-800 via-gray-500 to-gray-900 ">
+    <div className=" w-full pb-40 bg-gradient-to-tr from-gray-900 via-gray-500 to-gray-900 ">
       <div className="flex justify-center">
         <div className="bg-gray-800">
           <CLShomepage />
         </div>
       </div>
-      <div className={` ${isLoading? 'animate-fadeStep1 opacity-100':'opacity-0'} w-full grid place-items-center`}>
+      <div
+        className={` ${
+          isLoading ? "animate-fadeStep1 opacity-100" : "opacity-0"
+        } w-full grid place-items-center`}
+      >
         <h1 className="my-4 text-2xl lg:text-6xl text-white font-extrabold">
           IoT Device Management Platform
         </h1>
         <div className="px-4 text-sm text-center lg:text-2xl text-white w-full ">
-          &nbsp;&nbsp;&nbsp;&nbsp; A Customizable Web Interface for Seamless Control and Monitoriing
+          &nbsp;&nbsp;&nbsp;&nbsp; A Customizable Web Interface for Seamless
+          Control and Monitoriing
           <br />
           Manage IoT Devices anytime and anywhere in the wolrd
         </div>
@@ -30,14 +36,33 @@ export default function Home() {
       <div className="flex justify-center mb-4">
         <GetstartButton isLoading={isLoading} />
       </div>
-        <div className={` ${isLoading? 'animate-fadeStep1 opacity-100':'opacity-0'} mt-3 lg:mt-8 grid w-full place-items-center`}>
-               <div className="w-fit grid grid-cols-2 place-items-center gap-0">
-               <CardImageHomepage lable="Welcom" imgURL="/aboutus/aboutus_gabel.jpg"/>
-               <CardImageHomepage lable="To" imgURL="/aboutus/aboutus_bam.jpg"/>
-               <CardImageHomepage lable="IoT" imgURL="/aboutus/aboutus_por.jpg"/>
-               <CardImageHomepage lable="Platform" imgURL="/aboutus/watid.jpg"/>
-             </div>
+      <div
+        className={` ${
+          isLoading ? "animate-fadeStep1 opacity-100" : "opacity-0"
+        } mt-3 lg:mt-8 grid w-full place-items-center`}
+      >
+        <FadeInOnScroll>
+          <div className="w-fit  grid lg:grid-cols-2 md:grid-cols-2 place-items-center gap-0">
+            <CardImageHomepage lable="Welcom" imgURL="/aboutus/gabel.jpg" />
+            <CardImageHomepage lable="To" imgURL="/aboutus/bam.jpg" />
+            <CardImageHomepage lable="IoT" imgURL="/aboutus/por.jpg" />
+            <CardImageHomepage lable="Platform" imgURL="/aboutus/watid.jpg" />
+          </div>
+        </FadeInOnScroll>
+      </div>
+      <FadeInOnScroll>
+        <div className="grid my-5 lg:my-10 lg:grid-cols-[45%_55%]">
+          <div className="grid place-items-center">
+            <h1 className="text-3xl lg:text-5xl font-bold text-white text-center">
+              What is this Device
+              <br />
+              Management IoT Platform
+            </h1>
+            <img src="/homepage/iot.png" className="w-[400px] h-[400px]" />
+          </div>
+          <div className="grid lg:grid-cols-2 lg:gap-4 "></div>
         </div>
+      </FadeInOnScroll>
       <Homepage_article />
     </div>
   );
---
+++ src/components/FadInScroll.tsx
@@ -0,0 +1,46 @@
+import { useState, useEffect, ReactNode } from 'react';
+import { motion } from 'framer-motion';
+import React from 'react';
+
+interface FadeInOnScrollProps {
+    children: ReactNode; 
+  }
+
+const FadeInOnScroll: React.FC<FadeInOnScrollProps> = ({ children }) => {
+  const [isVisible, setIsVisible] = useState(false);
+  const ref = React.useRef(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) {
+          setIsVisible(true);
+        }
+      },
+      { threshold: 0.1 } 
+    );
+
+    if (ref.current) {
+      observer.observe(ref.current);
+    }
+
+    return () => {
+      if (ref.current) {
+        observer.unobserve(ref.current);
+      }
+    };
+  }, []);
+
+  return (
+    <motion.div
+      ref={ref}
+      initial={{ opacity: 0, y: 50 }} 
+      animate={isVisible ? { opacity: 1, y: 0 } : {}} 
+      transition={{ duration: 1.8, ease: 'easeOut' }}
+    >
+      {children}
+    </motion.div>
+  );
+};
+
+export default FadeInOnScroll;
