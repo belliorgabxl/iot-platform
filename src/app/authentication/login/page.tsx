f4376092098db22f28bfa38744972181615f911b
patarajarin <patara1919@gmail.com>
2025-01-27 10:24:58 +0700

    update login form

--- src/app/authentication/login/page.tsx
+++ src/app/authentication/login/page.tsx
@@ -43,18 +43,18 @@ export default function Page() {
   };
 
   return (
-    <div className="w-full grid place-items-center bg-white lg:bg-gradient-to-r lg:from-sky-100 lg:via-white lg:to-sky-100 py-5">
+    <div className="w-full grid place-items-center bg-gray-600 lg:bg-gradient-to-r  py-5">
       <div className="relative my-10  w-full max-w-3xl flex justify-center items-center">
-        <div className="hidden lg:block md:block absolute mix-blend-multiply filter blur-lg top-0 left-10 w-72 h-72  bg-sky-300 opacity-70 rounded-full animate-blob"></div>
-        <div className="hidden lg:block md:block absolute mix-blend-multiply filter blur-lg top-0 right-10 w-72 h-72  bg-yellow-300 opacity-60 rounded-full  animate-blob animation-delay-2000"></div>
-        <div className="hidden lg:block md:block absolute mix-blend-multiply filter blur-lg bottom-0 left-30 w-72 h-72 bg-pink-300 opacity-60 rounded-full  animate-blob animation-delay-4000"></div>
+        <div className="hidden lg:block md:block absolute mix-blend-multiply filter blur-sm top-0 left-10 w-72 h-72  bg-sky-300 opacity-90 rounded-full animate-blob"></div>
+        <div className="hidden lg:block md:block absolute mix-blend-multiply filter blur-sm top-0 right-10 w-72 h-72  bg-yellow-300 opacity-90 rounded-full  animate-blob animation-delay-2000"></div>
+        <div className="hidden lg:block md:block absolute mix-blend-multiply filter blur-sm bottom-0 left-30 w-72 h-72 bg-pink-300 opacity-90 rounded-full  animate-blob animation-delay-4000"></div>
 
-        <div className="lg:hidden md:hidden absolute mix-blend-multiply filter blur-lg top-0 left-10 w-60 h-60 bg-sky-300 opacity-70 rounded-full animate-blob"></div>
-        <div className="lg:hidden md:hidden absolute mix-blend-multiply filter blur-lg top-0 right-10 w-60 h-60  bg-yellow-300 opacity-60 rounded-full  animate-blob animation-delay-2000"></div>
-        <div className="lg:hidden md:hidden absolute mix-blend-multiply filter blur-lg bottom-0 left-30 w-60 h-60 bg-pink-300 opacity-60 rounded-full  animate-blob animation-delay-4000"></div>
+        <div className="lg:hidden md:hidden absolute mix-blend-multiply filter  blur-sm top-0 left-10 w-60 h-60 bg-sky-300 opacity-100 rounded-full animate-blob"></div>
+        <div className="lg:hidden md:hidden absolute mix-blend-multiply filter  blur-sm top-0 right-10 w-60 h-60  bg-blue-500 opacity-100 rounded-full  animate-blob animation-delay-2000"></div>
+        <div className="lg:hidden md:hidden absolute mix-blend-multiply filter  blur-sm bottom-0 left-30 w-60 h-60 bg-gray-300 opacity-100 rounded-full  animate-blob animation-delay-4000"></div>
 
-        <div className="w-fit grid z-10  bg-white  rounded-xl my-10 shadow-sm gap-5 ">
-          <div className="text-3xl py-1 rounded-t-xl bg-gradient-to-l from-blue-600 to-gray-900 font-medium text-white mb-2 text-center w-full">
+        <div className="lg:w-fit w-[90%] grid z-10  bg-white  rounded-xl my-10 shadow-sm gap-5 ">
+          <div className="text-3xl py-2 rounded-t-xl bg-gradient-to-l from-blue-600 to-gray-900 font-medium text-white mb-2 text-center w-full">
             Login
           </div>
           <div className="grid  place-items-center px-10 lg:px-20 gap-5 pb-5">
@@ -68,7 +68,7 @@ export default function Page() {
                 value={user.email}
                 onChange={(e) => setUser({ ...user, email: e.target.value })}
                 placeholder="Email"
-                className="border lg:w-auto w-[100px] px-4 py-1 border-gray-200 rounded-md bg-white text-gray-600 focus:outline-blue-500"
+                className="border lg:w-auto w-[180px] px-4 py-1 border-gray-200 rounded-md bg-white text-gray-600 focus:outline-blue-500"
               />
             </div>
             <div className="flex gap-4">
@@ -81,19 +81,19 @@ export default function Page() {
                 value={user.password}
                 onChange={(e) => setUser({ ...user, password: e.target.value })}
                 placeholder="Password"
-                className="border px-4 py-1 lg:w-auto w-[100px] border-gray-200 rounded-md bg-white text-gray-600 focus:outline-blue-500"
+                className="border px-4 py-1 lg:w-auto w-[180px] border-gray-200 rounded-md bg-white text-gray-600 focus:outline-blue-500"
               />
             </div>
             <div className="flex gap-2 lg:gap-10 my-4">
               <button
-                className="px-4 lg:px-10 md:px-10 py-1 bg-blue-300 enabled:bg-blue-500 enabled:hover:bg-blue-400 text-white rounded-md"
+                className="px-8 lg:px-10 md:px-10 py-1 bg-gradient-to-l from-blue-600 to-gray-700 opacity-70 enabled:opacity-100 hover:opacity-80 text-white rounded-md"
                 onClick={onLogin}
                 disabled={!user.email || !user.password}
               >
                 Login
               </button>
               <button
-                className="px-4 lg:px-10 md:px-10 py-1  hover:bg-blue-400 hover:text-white  text-blue-500 rounded-md"
+                className="px-8 lg:px-10 md:px-10 py-1  hover:bg-blue-400 hover:text-white  text-blue-500 rounded-md"
                 onClick={() => {
                   router.push("/authentication/signup");
                 }}
--- src/app/authentication/signup/page.tsx
+++ src/app/authentication/signup/page.tsx
@@ -23,85 +23,87 @@ export default function Page() {
         toast.error("sign-up failed");
       }
     }
-    if (!user.username){
-      toast.error("please provide username")
+    if (!user.username) {
+      toast.error("please provide username");
     }
-    if (!user.password){
-      toast.error("please provide password")
+    if (!user.password) {
+      toast.error("please provide password");
     }
-    if (!user.email){
-      toast.error("please provide email")
+    if (!user.email) {
+      toast.error("please provide email");
     }
   };
 
   return (
-    <div className="w-full grid place-items-center  lg:bg-gradient-to-r lg:from-sky-100 lg:via-white lg:to-sky-100 py-5">
+    <div className="w-full grid place-items-center  bg-gray-600  py-5">
       <div className="relative w-full max-w-3xl flex justify-center items-center">
-      <div className="hidden lg:block md:block absolute mix-blend-multiply filter blur-lg top-0 left-10 w-72 h-72  bg-sky-300 opacity-70 rounded-full animate-blob"></div>
-        <div className="hidden lg:block md:block absolute mix-blend-multiply filter blur-lg top-0 right-10 w-72 h-72  bg-yellow-300 opacity-60 rounded-full  animate-blob animation-delay-2000"></div>
-        <div className="hidden lg:block md:block absolute mix-blend-multiply filter blur-lg bottom-0 left-30 w-72 h-72 bg-pink-300 opacity-60 rounded-full  animate-blob animation-delay-4000"></div>
+      <div className="hidden lg:block md:block absolute mix-blend-multiply filter blur-sm top-0 left-10 w-72 h-72  bg-sky-300 opacity-90 rounded-full animate-blob"></div>
+        <div className="hidden lg:block md:block absolute mix-blend-multiply filter blur-sm top-0 right-10 w-72 h-72  bg-yellow-300 opacity-90 rounded-full  animate-blob animation-delay-2000"></div>
+        <div className="hidden lg:block md:block absolute mix-blend-multiply filter blur-sm bottom-0 left-30 w-72 h-72 bg-pink-300 opacity-90 rounded-full  animate-blob animation-delay-4000"></div>
 
-        <div className="lg:hidden md:hidden absolute mix-blend-multiply filter blur-lg top-0 left-10 w-60 h-60 bg-sky-300 opacity-70 rounded-full animate-blob"></div>
-        <div className="lg:hidden md:hidden absolute mix-blend-multiply filter blur-lg top-0 right-10 w-60 h-60  bg-yellow-300 opacity-60 rounded-full  animate-blob animation-delay-2000"></div>
-        <div className="lg:hidden md:hidden absolute mix-blend-multiply filter blur-lg bottom-0 left-30 w-60 h-60 bg-pink-300 opacity-60 rounded-full  animate-blob animation-delay-4000"></div>
-        <div className="w-fit grid z-10 place-items-start bg-white  rounded-md my-10 shadow-sm gap-5 px-10 py-5">
-          <div className="text-3xl text-black mb-2 text-center w-full">
+        <div className="lg:hidden md:hidden absolute mix-blend-multiply filter  blur-sm top-0 left-10 w-60 h-60 bg-sky-300 opacity-100 rounded-full animate-blob"></div>
+        <div className="lg:hidden md:hidden absolute mix-blend-multiply filter  blur-sm top-0 right-10 w-60 h-60  bg-blue-500 opacity-100 rounded-full  animate-blob animation-delay-2000"></div>
+        <div className="lg:hidden md:hidden absolute mix-blend-multiply filter  blur-sm bottom-0 left-30 w-60 h-60 bg-gray-300 opacity-100 rounded-full  animate-blob animation-delay-4000"></div>
+        <div className="lg:w-fit w-[90%] grid z-10  bg-white  rounded-xl my-10 shadow-sm gap-5 ">
+          <div className="text-3xl py-2 rounded-t-xl bg-gradient-to-l from-blue-600 to-gray-900 font-medium text-white mb-2 text-center w-full">
             Sign-Up
           </div>
-          <div className="flex gap-4">
-            <label className="w-[80px]" htmlFor="username">
-              Username
-            </label>
-            <input
-              type="text"
-              id="username"
-              value={user.username}
-              onChange={(e) => setUser({ ...user, username: e.target.value })}
-              placeholder="username"
-              className="border w-[120px] lg:w-auto px-4 py-1 border-gray-200 rounded-md bg-white text-gray-600 focus:outline-blue-500"
-            />
-          </div>
-          <div className="flex gap-4">
-            <label className="w-[80px]" htmlFor="email">
-              Email
-            </label>
-            <input
-              type="text"
-              id="email"
-              value={user.email}
-              onChange={(e) => setUser({ ...user, email: e.target.value })}
-              placeholder="email"
-              className="border w-[120px] lg:w-auto px-4 py-1 border-gray-200 rounded-md bg-white text-gray-600 focus:outline-blue-500"
-            />
-          </div>
-          <div className="flex gap-4">
-            <label className="w-[80px]" htmlFor="password">
-              Password
-            </label>
-            <input
-              type="password"
-              id="password"
-              value={user.password}
-              onChange={(e) => setUser({ ...user, password: e.target.value })}
-              placeholder="password"
-              className="border w-[120px] lg:w-auto px-4 py-1 border-gray-200 rounded-md bg-white text-gray-600 focus:outline-blue-500"
-            />
-          </div>
-          <div className="flex gap-2 lg:gap-10 my-4">
-            <button
-              className="lg:px-10 md:px-10 px-4 py-1 bg-blue-500 hover:bg-blue-300 text-white rounded-md"
-              onClick={onSignUp}
-            >
-              Sign-Up
-            </button>
-            <button
-              className="lg:px-10 md:px-10 px-4 py-1 hover:bg-gray-300 hover:text-white  text-blue-500 rounded-md"
-              onClick={() => {
-                router.push("/authentication/login");
-              }}
-            >
-              Login
-            </button>
+          <div className="grid  place-items-center px-10 lg:px-20 gap-5 pb-5">
+            <div className="flex gap-4">
+              <label className="w-[80px]" htmlFor="username">
+                Username
+              </label>
+              <input
+                type="text"
+                id="username"
+                value={user.username}
+                onChange={(e) => setUser({ ...user, username: e.target.value })}
+                placeholder="username"
+                className="border w-[160px] lg:w-auto px-4 py-1 border-gray-200 rounded-md bg-white text-gray-600 focus:outline-blue-500"
+              />
+            </div>
+            <div className="flex gap-4">
+              <label className="w-[80px]" htmlFor="email">
+                Email
+              </label>
+              <input
+                type="text"
+                id="email"
+                value={user.email}
+                onChange={(e) => setUser({ ...user, email: e.target.value })}
+                placeholder="email"
+                className="border w-[160px] lg:w-auto px-4 py-1 border-gray-200 rounded-md bg-white text-gray-600 focus:outline-blue-500"
+              />
+            </div>
+            <div className="flex gap-4">
+              <label className="w-[80px]" htmlFor="password">
+                Password
+              </label>
+              <input
+                type="password"
+                id="password"
+                value={user.password}
+                onChange={(e) => setUser({ ...user, password: e.target.value })}
+                placeholder="password"
+                className="border w-[160px] lg:w-auto px-4 py-1 border-gray-200 rounded-md bg-white text-gray-600 focus:outline-blue-500"
+              />
+            </div>
+            <div className="flex gap-2 lg:gap-10 my-4">
+              <button
+                className="lg:px-10 md:px-10 px-4 py-1 bg-blue-500 hover:bg-blue-300 text-white rounded-md"
+                onClick={onSignUp}
+              >
+                Sign-Up
+              </button>
+              <button
+                className="lg:px-10 md:px-10 px-4 py-1 hover:bg-gray-300 hover:text-white  text-blue-500 rounded-md"
+                onClick={() => {
+                  router.push("/authentication/login");
+                }}
+              >
+                Login
+              </button>
+            </div>
           </div>
         </div>
       </div>
