ac426e2890b8bd177a58f8858a93425599ac09dc
patarajarin <patara1919@gmail.com>
2025-01-27 09:09:15 +0700

    edit device page (button add)

--- src/app/devices/Body.tsx
+++ src/app/devices/Body.tsx
@@ -280,7 +280,7 @@ export default function Body({ session }: Props) {
             </div>
           </div>
 
-          <div className="lg:flex lg:justify-end sm:grid sm:place-items-end grid place-items-end gap-3">
+          <div className="lg:flex lg:justify-end sm:grid sm:place-items-end grid place-items-start gap-3">
             <button
               className="flex gap-2 justify-center bg-blue-600 rounded-md items-center hover:bg-slate-600  shadow-md  h-fit text-white w-[180px] px-2 text-sm lg:px-4 py-2 "
               onClick={onClickPopUp}
