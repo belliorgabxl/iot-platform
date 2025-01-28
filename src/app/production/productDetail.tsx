8eca99ca7b228591fe6124a10f5cb54a86c16074
patarajarin <patara1919@gmail.com>
2025-01-28 17:48:32 +0700

    adding production smoke-detect

--- src/app/production/productDetail.tsx
+++ src/app/production/productDetail.tsx
@@ -20,7 +20,7 @@ export default function ProductDetail({ isLoading }: Props) {
           className="lg:w-1/3  md:w-2/5 sm:w-[250px] w-1/3 rounded-l-lg object-cover "
         />
         <div className="lg:px-5 px-2 grid gap-2 py-2 w-full">
-          <h1 className="lg:text-2xl text-sm  bg-gray-900 h-fit px-2 lg:px-5  grid place-items-center py-1 rounded-md">
+          <h1 className="lg:text-2xl text-lg  bg-gray-900 h-fit px-2 lg:px-5  grid place-items-center py-1 rounded-md">
             Product : Car
           </h1>
           <p className="lg:text-lg text-sm">Board : ESP32</p>
@@ -45,7 +45,7 @@ export default function ProductDetail({ isLoading }: Props) {
         />
 
         <div className="lg:px-5 px-2 grid gap-2 py-2 w-full">
-          <div className="text-sm lg:text-xl  bg-gray-900 h-fit px-2 lg:px-5 grid place-items-center py-1 overflow-hidden text-ellipsis whitespace-nowrap line-clamp-1 rounded-md">
+          <div className="text-lg lg:text-xl  bg-gray-900 h-fit px-2 lg:px-5 grid place-items-center py-1 overflow-hidden text-ellipsis whitespace-nowrap line-clamp-1 rounded-md">
             Product : Robotic Arm
           </div>
           <p className="text-sm lg:text-lg">Board : ESP32</p>
@@ -71,8 +71,35 @@ export default function ProductDetail({ isLoading }: Props) {
         />
 
         <div className="lg:px-5 px-2 grid gap-2 py-2 w-full">
-          <div className="text-sm lg:text-xl  bg-gray-900 h-fit px-2 lg:px-5 grid place-items-center py-1 overflow-hidden text-ellipsis whitespace-nowrap line-clamp-1 rounded-md">
+          <div className="text-lg lg:text-xl  bg-gray-900 h-fit px-2 lg:px-5 grid place-items-center py-1 overflow-hidden text-ellipsis whitespace-nowrap line-clamp-1 rounded-md">
             Product : Auto Pump
+          </div>
+          <p className=" text-sm lg:text-lg sm:text-lg">Board : ESP32</p>
+          <p className="text-sm lg:text-lg sm:text-lg">Connection Type : Wi-fi 2.4 GHz</p>
+          <p className=" grid place-content-center rounded-md py-1 w-fit px-2 lg:px-10 bg-gray-500 text-white  text-sm sm:text-lg lg:text-xl">
+            Inventory : 1 pc.
+          </p>
+        </div>
+      </div>
+
+      <div
+        className={`flex h-fit gap-1 ${
+          isLoading
+            ? "bg-gray-800  hover:scale-[102%] duration-1000  text-white rounded-lg   "
+            : "opacity-0"
+        }`}
+      >
+        <img
+          src="/effect/smoke-detection.jpg"
+          width="full"
+          height="full"
+          alt="pump"
+          className="lg:w-1/3  md:w-2/5 sm:w-[250px] w-1/3 rounded-l-lg object-cover "
+        />
+
+        <div className="lg:px-5 px-2 grid gap-2 py-2 w-full">
+          <div className="text-lg lg:text-xl  bg-gray-900 h-fit px-2 lg:px-5 grid place-items-center py-1 overflow-hidden text-ellipsis whitespace-nowrap line-clamp-1 rounded-md">
+            Product : Smoke Detector
           </div>
           <p className=" text-sm lg:text-lg sm:text-lg">Board : ESP32</p>
           <p className="text-sm lg:text-lg sm:text-lg">Connection Type : Wi-fi 2.4 GHz</p>
