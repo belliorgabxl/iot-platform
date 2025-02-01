2f7aa1d71249af81f4e6990a12e8bc62ac22a3f2
patarajarin <patara1919@gmail.com>
2025-02-01 14:56:43 +0700

    update pocket

--- src/components/pockets/email.tsx
+++ src/components/pockets/email.tsx
@@ -5,7 +5,7 @@ import React from "react";
 
 export default function EmialPocket() {
   return (
-    <Link href="/documents/help" className="fixed group z-20 right-4 bottom-4">
+    <Link href="/documents/help" className="fixed group z-20 md:right-24 right-4 lg:right-24  md:bottom-4 bottom-24 :lg:bottom-4">
       <div className="overflow-hidden flex items-center justify-center relative  rounded-full h-16 w-16  bg-white/30 backdrop-blur-sm group-hover:opacity-70 group-hover:scale-[105%] text-white">
         <Mail style={{ width: "2.0rem", height: "2.0rem" }} />
       </div>
