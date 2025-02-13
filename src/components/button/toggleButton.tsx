c32cc5bffabbc9c142d04839592b00d60b384a4b
chanidaphach28 <chanidapha.28chsk@gmail.com>
2025-02-13 23:17:31 +0700

    Add custom button Icon & Png

--- src/components/button/toggleButton.tsx
+++ src/components/button/toggleButton.tsx
@@ -28,62 +28,111 @@ const ToggelButton = ({
 
   const Control = async (cmd: string) => {
     if (client && isConnected) {
-      if (toggle == false) {
+      if (!toggle) {
         client.publish(topic, `ctrl/${cmd}`);
         console.log(`Command : ${cmd.toUpperCase()}`);
         onLogReturn(`Command : ${cmd.toUpperCase()}`);
-      }else if (toggle == true){
+      } else {
         client.publish(topic, `ctrl/stop`);
-        console.log(`Command : STOP`);
-        onLogReturn(`Command : STOP`);
+        console.log("Command : STOP");
+        onLogReturn("Command : STOP");
       }
       setToggle(!toggle);
     }
   };
 
-  const handlePush = () => {
-    setToggle(!toggle);
-  };
   return (
     <button
-      onClick={() => {
-        Control(cmd);
-      }}
+      onClick={() => Control(cmd)}
       className={`${
-        toggle == true
-          ? "bg-pink-300 border-2 border-pink-500 text-black shadow-black shadow-inner  scale-[95%]"
-          : "bg-pink-600 border-2 border-pink-500  shadow-md shadow-black text-white"
-      }  rounded-xl  px-3 py-3 text-center hover:opacity-90  `}
+        toggle
+          ? "bg-pink-300 border-2 border-pink-500 text-black shadow-black shadow-inner scale-[95%]"
+          : "bg-pink-600 border-2 border-pink-500 shadow-md shadow-black text-white"
+      } rounded-xl px-3 py-3 text-center hover:opacity-90`}
     >
-      {label == "Up" || label == "Forward" ? (
+      {label === "Up" || label === "Forward" ? (
         <img
           src={`${
-            toggle ? "/png_icon/forwardblack.png" : "/png_icon/forwardwhite.png"
-          } `}
+            toggle
+              ? "/png_icon/forwardblack.png"
+              : "/png_icon/forwardwhite.png"
+          }`}
           width={50}
           height={50}
         />
-      ) : label == "Down" || label == "Backward" ? (
+      ) : label === "Down" || label === "Backward" ? (
         <img
-          src={`${toggle ? "/png_icon/backblack.png" : "/png_icon/backwhite.png"} `}
+          src={`${
+            toggle ? "/png_icon/backblack.png" : "/png_icon/backwhite.png"
+          }`}
           width={50}
           height={50}
         />
-      ) : label == "Left" ? (
+      ) : label === "Left" ? (
         <img
-          src={`${toggle ? "/png_icon/leftblack.png" : "/png_icon/leftwhite.png"} `}
+          src={`${
+            toggle ? "/png_icon/leftblack.png" : "/png_icon/leftwhite.png"
+          }`}
           width={50}
           height={50}
         />
-      ) : label == "Right" ? (
+      ) : label === "Right" ? (
         <img
           src={`${
             toggle ? "/png_icon/rightblack.png" : "/png_icon/rightwhite.png"
-          } `}
+          }`}
           width={50}
           height={50}
         />
-      ) : (
+      ) : label === "ON" ? (
+        <img
+          src={`${
+            toggle ? "/png_icon/ongreen.png" : "/png_icon/offwhite.png"
+          }`}
+          width={50}
+          height={50}
+        />
+      ) : label === "Alarm" ? (
+        <img
+          src={`${
+            toggle ? "/png_icon/bellon.png" : "/png_icon/belloff.png"
+          }`}
+          width={50}
+          height={50}
+        />
+      ): label === "Locked" ? (
+        <img
+          src={`${
+            toggle ? "/png_icon/lock.png" : "/png_icon/unlock.png"
+          }`}
+          width={50}
+          height={50}
+        />
+      ): label === "Warning" ? (
+        <img
+          src={`${
+            toggle ? "/png_icon/warning.png" : "/png_icon/warningoff.png"
+          }`}
+          width={50}
+          height={50}
+        />
+      ): label === "Record" ? (
+        <img
+          src={`${
+            toggle ? "/png_icon/recodeon.png" : "/png_icon/recodeoff.png"
+          }`}
+          width={50}
+          height={50}
+        />
+      ): label === "Sound ON" ? (
+        <img
+          src={`${
+            toggle ? "/png_icon/soundoff.png" : "/png_icon/soundon.png"
+          }`}
+          width={50}
+          height={50}
+        />
+      ): (
         <div>None</div>
       )}
     </button>
